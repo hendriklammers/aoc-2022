21ef4ebdd3ac92fa5451b7abb4cfe7fd94249fb7
Hendrik Lammers <lammers@gmail.com>
2022-12-02 00:09:08 +0800

    Add day 1 part 2

---
+++ src/day1.rs
@@ -0,0 +1,60 @@
+use std::env;
+use std::fs;
+
+fn read_file() -> String {
+    let file_path = env::current_dir().unwrap().join("inputs/day-1.txt");
+    fs::read_to_string(file_path).expect("Unable to open input file")
+}
+
+fn part1(input: &String) {
+    let mut most = 0;
+    let mut current = 0;
+
+    for line in input.lines() {
+        match line.parse::<i32>() {
+            Ok(amount) => {
+                current = current + amount;
+            }
+            Err(_) => {}
+        }
+        if line == "" {
+            if current > most {
+                most = current;
+            }
+            current = 0;
+        }
+    }
+    println!("Part 1: {}", most);
+}
+
+fn part2(input: &String) {
+    let mut totals = vec![];
+    let mut current = 0;
+
+    for line in input.lines() {
+        match line.parse::<i32>() {
+            Ok(amount) => {
+                current = current + amount;
+            }
+            Err(_) => {}
+        }
+        if line == "" {
+            totals.push(current);
+            current = 0;
+        }
+    }
+
+    totals.sort();
+    totals.reverse();
+
+    let result: i32 = (&totals[..3]).iter().sum();
+
+    println!("Part 2: {}", result);
+}
+
+pub fn solve() {
+    let input = read_file();
+
+    part1(&input);
+    part2(&input);
+}
